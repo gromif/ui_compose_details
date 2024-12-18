a990d74924552f756536a58c7c6ac1bda37c7b92
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-19 01:16:42 +0200

    Change JVM-Target to project var

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,13 +20,10 @@ android {
             isMinifyEnabled = false
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+}
+
+kotlin {
+    jvmToolchain(project.property("kotlinJvmToolchainVersion").toString().toInt())
 }
 
 dependencies {
