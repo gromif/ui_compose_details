60150a59fc03d75790e179a184cef25e9579e2b2
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-22 13:25:25 +0200

    Add AndroidLibrary convention plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,16 @@
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.astracrypt.android.library)
     alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.nevidimka655.compose_details"
-    compileSdk = project.property("compileSdk").toString().toInt()
 
     defaultConfig {
-        minSdk = project.property("minSdk").toString().toInt()
         consumerProguardFiles("consumer-rules.pro")
 
         buildFeatures.compose = true
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-}
-
-kotlin {
-    jvmToolchain(project.property("kotlinJvmToolchainVersion").toString().toInt())
 }
 
 dependencies {
